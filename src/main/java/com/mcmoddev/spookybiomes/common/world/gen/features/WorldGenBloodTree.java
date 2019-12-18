505137ebed8243d2cf2d72dd1a9a3d3942edf4df
ProxyNeko <proxyneko@users.noreply.github.com>
2019-12-18 04:47:01 +0000

    Time for a curseforge push.

--- src/main/java/com/mcmoddev/spookybiomes/common/world/gen/features/WorldGenBloodTree.java
+++ src/main/java/com/mcmoddev/spookybiomes/common/world/gen/features/WorldGenBloodTree.java
@@ -99,3 +99,109 @@ public class WorldGenBloodTree extends WorldGenAbstractTree {
         }
     }
 }
+
+/*
+//TODO This in future.. Crashes on startup with the leaves in the super.
+private final boolean useBaseHeight;
+
+    public WorldGenBloodTree(boolean notify, boolean p_i45457_2_) {
+        super(notify, 13, 15, SpookyBlockObjects.BLOODWOOD_LOG.getDefaultState(),
+                SpookyBlockObjects.BLOODWOOD_LEAVES.getDefaultState().withProperty(BlockLeaves.CHECK_DECAY, false));
+        this.useBaseHeight = p_i45457_2_;
+    }
+
+    public boolean generate(World worldIn, Random rand, BlockPos position) {
+        int i = this.getHeight(rand);
+
+        if (!this.ensureGrowable(worldIn, rand, position, i)) {
+            return false;
+        } else {
+            this.createCrown(worldIn, position.getX(), position.getZ(), position.getY() + i, 0, rand);
+
+            for (int j = 0; j < i; ++j) {
+                if (isAirLeaves(worldIn, position.up(j))) {
+                    this.setBlockAndNotifyAdequately(worldIn, position.up(j), this.woodMetadata);
+                }
+
+                if (j < i - 1) {
+                    if (isAirLeaves(worldIn, position.add(1, j, 0))) {
+                        this.setBlockAndNotifyAdequately(worldIn, position.add(1, j, 0), this.woodMetadata);
+                    }
+
+                    if (isAirLeaves(worldIn, position.add(1, j, 1))) {
+                        this.setBlockAndNotifyAdequately(worldIn, position.add(1, j, 1), this.woodMetadata);
+                    }
+
+
+                    if (isAirLeaves(worldIn, position.add(0, j, 1))) {
+                        this.setBlockAndNotifyAdequately(worldIn, position.add(0, j, 1), this.woodMetadata);
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    private void createCrown(World worldIn, int x, int z, int y, int p_150541_5_, Random rand) {
+        int i = rand.nextInt(5) + (this.useBaseHeight ? this.baseHeight : 3);
+        int j = 0;
+
+        for (int k = y - i; k <= y; ++k) {
+            int l = y - k;
+            int i1 = p_150541_5_ + MathHelper.floor((float) l / (float) i * 3.5F);
+            this.growLeavesLayerStrict(worldIn, new BlockPos(x, k, z), i1 + (l > 0 && i1 == j && (k & 1) == 0 ? 1 : 0));
+            j = i1;
+        }
+    }
+
+    public void generateSaplings(World worldIn, Random random, BlockPos pos) {
+        this.placeBloodiedGrassCircle(worldIn, pos.west().north());
+        this.placeBloodiedGrassCircle(worldIn, pos.east(2).north());
+        this.placeBloodiedGrassCircle(worldIn, pos.west().south(2));
+        this.placeBloodiedGrassCircle(worldIn, pos.east(2).south(2));
+
+        for (int i = 0; i < 5; ++i) {
+            int j = random.nextInt(64);
+            int k = j % 8;
+            int l = j / 8;
+
+            if (k == 0 || k == 7 || l == 0 || l == 7) {
+                this.placeBloodiedGrassCircle(worldIn, pos.add(-3 + k, 0, -3 + l));
+            }
+        }
+    }
+
+    private void placeBloodiedGrassCircle(World worldIn, BlockPos center) {
+        for (int i = -2; i <= 2; ++i) {
+            for (int j = -2; j <= 2; ++j) {
+                if (Math.abs(i) != 2 || Math.abs(j) != 2) {
+                    this.placeBloodiedGrassAt(worldIn, center.add(i, 0, j));
+                }
+            }
+        }
+    }
+
+    private void placeBloodiedGrassAt(World worldIn, BlockPos pos) {
+        for (int i = 2; i >= -3; --i) {
+            BlockPos blockpos = pos.up(i);
+            IBlockState iblockstate = worldIn.getBlockState(blockpos);
+            Block block = iblockstate.getBlock();
+
+            if (block.canSustainPlant(iblockstate, worldIn, blockpos, EnumFacing.UP, ((BlockSapling) Blocks.SAPLING))) {
+                this.setBlockAndNotifyAdequately(worldIn, blockpos, SpookyBlockObjects.BLOODIED_GRASS.getDefaultState());
+                break;
+            }
+
+            if (iblockstate.getMaterial() != Material.AIR && i < 0) {
+                break;
+            }
+        }
+    }
+
+    //Helper macro
+    private boolean isAirLeaves(World world, BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+        return state.getBlock().isAir(state, world, pos) || state.getBlock().isLeaves(state, world, pos);
+    }
+ */
