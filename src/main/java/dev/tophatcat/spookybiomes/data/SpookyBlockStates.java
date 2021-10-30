91fb28936060ab0d566cbe8d13429e9c3b1ad8a6
sciwhiz12 <arnoldnunag12@gmail.com>
2021-10-30 09:22:38 +0800

    Add datagen for bloodied grass
    Notable changes:
     - The individual UVs for each face in the non-snowy model are not
    included in the generated JSON, as they match the default UVs.
     - The particle texture used in the snowy model is changed to the
    bloodied dirt texture, to match the non-snowy model.

--- src/main/java/dev/tophatcat/spookybiomes/data/SpookyBlockStates.java
+++ src/main/java/dev/tophatcat/spookybiomes/data/SpookyBlockStates.java
@@ -1,11 +1,15 @@
 package dev.tophatcat.spookybiomes.data;
 
 import dev.tophatcat.spookybiomes.SpookyBiomes;
+import dev.tophatcat.spookybiomes.common.blocks.BloodiedGrass;
 import dev.tophatcat.spookybiomes.init.SpookyBlocks;
+import net.minecraft.core.Direction;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 public class SpookyBlockStates extends BlockStateProvider {
@@ -21,5 +25,44 @@ public class SpookyBlockStates extends BlockStateProvider {
         getVariantBuilder(SpookyBlocks.BLOODIED_DIRT.get())
             .forAllStates(state -> ConfiguredModel.allYRotations(bloodiedDirtModel, 0, false));
         itemModels().getBuilder("bloodied_dirt").parent(bloodiedDirtModel);
+
+        bloodiedGrass();
+    }
+
+    private void bloodiedGrass() {
+        final BlockModelBuilder regularModel = models().withExistingParent("bloodied_grass", mcLoc("block/block"))
+            .texture("particle", modBlockTexture("bloodied_dirt"))
+            .texture("bottom", modBlockTexture("bloodied_dirt"))
+            .texture("top", modBlockTexture("bloodied_grass_top"))
+            .texture("side", modBlockTexture("bloodied_grass_sides"))
+            // Only one element: from 0,0,0 to 16,16,16
+            .element().from(0F, 0F, 0F).to(16F, 16F, 16F)
+            .face(Direction.DOWN).texture("#bottom").end()
+            .face(Direction.UP).texture("#top").tintindex(0).end()
+            .face(Direction.NORTH).texture("#side").end()
+            .face(Direction.SOUTH).texture("#side").end()
+            .face(Direction.WEST).texture("#side").end()
+            .face(Direction.EAST).texture("#side").end()
+            // All face have the same UVs (0,0 to 16,16), and the cullface is in the same direction
+            .allFaces((dir, face) -> face.uvs(0F, 0F, 16F, 16F).cullface(dir))
+            .end();
+
+        final BlockModelBuilder snowyModel = models().cubeBottomTop("bloodied_grass_snow",
+                modBlockTexture("bloodied_grass_snowy"), // side texture
+                modBlockTexture("bloodied_dirt"), // bottom texture
+                modBlockTexture("bloodied_grass_top")) // top texture
+            .texture("particle", modBlockTexture("bloodied_dirt"));
+
+        getVariantBuilder(SpookyBlocks.BLOODIED_GRASS.get())
+            .partialState().with(BloodiedGrass.SNOWY, false) // For the non-snowy variant
+            .setModels(ConfiguredModel.allYRotations(regularModel, 0, false))
+            .partialState().with(BloodiedGrass.SNOWY, true) // For the snowy variant
+            .setModels(ConfiguredModel.builder().modelFile(snowyModel).build());
+
+        itemModels().getBuilder("bloodied_grass").parent(regularModel);
+    }
+
+    public ResourceLocation modBlockTexture(String path) {
+        return modLoc(ModelProvider.BLOCK_FOLDER + "/" + path);
     }
 }
